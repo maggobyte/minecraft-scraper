8c81565dff724342caacce2a3503880949572e7b
maggobyte <magnust97@gmail.com>
2025-07-22 17:46:25 +0200

    Update MinecraftExporter initialization to use environment variables for host and interval

--- app/run.py
+++ app/run.py
@@ -5,5 +5,5 @@ host = os.getenv("MC_HOST", "localhost")
 port = int(os.getenv("MC_PORT", 25565))
 interval = int(os.getenv("MC_INTERVAL", 30))
 
-exporter = MinecraftExporter("10.15.15.4", 25565, interval=10)
+exporter = MinecraftExporter("localhost", 25565, interval=30)
 exporter.run()
